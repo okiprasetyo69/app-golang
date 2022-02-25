cc040cebcfa14800d71e57fc6310838fa06f11ff
okiprasetyo69 <oki.prasetyo45@gmail.com>
2022-02-25 14:40:04 +0700

    First app

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/okiprasetyo69/app-golang
+
+go 1.17
+
+require github.com/okiprasetyo69/golang-module/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/okiprasetyo69/golang-module/v2"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("Oki Prasetyo"))
+}
